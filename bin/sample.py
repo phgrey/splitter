174acce720a740500dc9d464708ec01d50c99b32
phgrey <phgrey@gmail.com>
2015-09-08 01:38:50 +0300

    a bit nice

--- bin/sample.py
+++ bin/sample.py
@@ -4,13 +4,14 @@ from lib.split import Split
 from lib.finder import wnds_by_pids
 
 
-applets = Applet.commands({'terminator', 'thunar'})
-apps = {x.process.pid: x for x in applets}
-try:
-    wnds_by_pids(set(apps.keys()), lambda pid, w: apps[pid].set_window(w))
-    print('all found')
-    Split().loop()
-except KeyboardInterrupt: pass
-finally:
-    for app in applets:
-        app.process.terminate()
+if __name__ == '__main__':
+    applets = Applet.commands({'terminator', 'thunar'})
+    apps = {x.process.pid: x for x in applets}
+    try:
+        wnds_by_pids(set(apps.keys()), lambda pid, w: apps[pid].set_window(w))
+        print('all found')
+        Split().loop()
+    except KeyboardInterrupt: pass
+    finally:
+        for app in applets:
+            app.process.terminate()
--- lib/split.py
+++ lib/split.py
@@ -1,23 +1,20 @@
-from lib.protocol import X, display, request
-from select import select
+from .protocol import X, display, request
+
 
 class Split:
-    def __init__(self, displ=None, msg='Hello world'):
-        self.display = displ or display
-        self.msg = msg
-
+    def __init__(self):
+        self.display = display
         self.screen = self.display.screen()
         self.window = self.screen.root.create_window(
             10, 10, 100, 100, 1,
             self.screen.root_depth,
             background_pixel=self.screen.white_pixel,
             event_mask=X.ExposureMask | X.KeyPressMask,
-            )
+        )
         self.gc = self.window.create_gc(
             foreground = self.screen.black_pixel,
             background = self.screen.white_pixel,
-            )
-        # request.StoreName(self.display, self.window.id, 'Hi! it\'s me')
+        )
         self.window.map()
 
     def loop(self):
@@ -25,19 +22,3 @@ class Split:
             e = self.display.next_event()
             if e.type == X.KeyPress:
                 raise SystemExit
-
-
-    def loop2(self):
-        readable = [self.display]
-        while True:
-            if readable and self.display in readable:
-                e = self.display.next_event()
-                if e.type == X.KeyPress:
-                    raise SystemExit
-                print('readable')
-            else:
-                print('nonreadable')
-            readable, w, e = select([self.display], [], [], 1)
-
-if __name__ == "__main__":
-    Split().loop()
